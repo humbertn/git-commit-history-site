81315cb6daca3d2859bb3919746a3f858c8cc938
Gerson Estrada <gersonestrada.giron@gmail.com>
2023-09-24 09:52:16 -0600

    Adding Header unit tests

---
+++ src/test/components/Header.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Header from '../../components/Header';
+
+describe('Header Component', () => {
+    it('renders the Navbar with the correct brand text', () => {
+      const { getByText } = render(<Header />);
+      const brandElement = getByText('Gerson Estrada');
+      expect(brandElement).toBeInTheDocument();
+    });
+  
+    it('renders the Navbar with the correct background color', () => {
+      const { container } = render(<Header />);
+      const navbar = container.querySelector('.navbar.bg-primary');
+      expect(navbar).toBeInTheDocument();
+    });
+  
+    it('renders the Navbar with dark theme', () => {
+      const { container } = render(<Header />);
+      const navbar = container.querySelector('[data-bs-theme="dark"]');
+      expect(navbar).toBeInTheDocument();
+    });
+  
+    it('renders the Navbar inside a Container', () => {
+      const { container } = render(<Header />);
+      const containerElement = container.querySelector('.container');
+      expect(containerElement).toBeInTheDocument();
+    });
+  
+    it('matches the snapshot', () => {
+      const { container } = render(<Header />);
+      expect(container).toMatchSnapshot();
+    });
+});
+  
